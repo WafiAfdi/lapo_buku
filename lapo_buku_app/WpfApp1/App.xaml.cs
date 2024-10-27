e473aa269a1ec5f363b7d1035c5fc592235aa2a5
Wafi Afdi Alfaruqhi <wafiafdi@gmail.com>
2024-10-27 20:12:02 +0700

    setup : package pgsql dan env

--- lapo_buku_app/WpfApp1/App.xaml.cs
+++ lapo_buku_app/WpfApp1/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetEnv;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,13 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Env.Load();  // Loads .env file
+
+            // Optional: Test if it's loaded correctly
+            Console.WriteLine(Environment.GetEnvironmentVariable("DB_HOST"));
+        }
     }
 }
---
+++ lapo_buku_app/WpfApp1/Service/DbPgService.cs
@@ -0,0 +1,42 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Service
+{
+    internal class DbPgService
+    {
+        private string _connectionString;
+
+        public DbPgService()
+        {
+            // Build the connection string using environment variables
+            _connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                                $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                                $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+                                $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+                                $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+        }
+
+        public DataTable GetData(string query)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand(query, connection))
+                {
+                    var dt = new DataTable();
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        dt.Load(reader);
+                    }
+                    return dt;
+                }
+            }
+        }
+    }
+}
