83bdf6ae117b2d835e6e19e891f0ed220e075479
Wafi Afdi Alfaruqhi <wafiafdi@gmail.com>
2024-11-03 19:57:39 +0700

    setup untuk navigation

---
+++ lapo_buku_app/WpfApp1/Models/ModelBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class ModelBase : INotifyPropertyChanged
+    {
+        // Event required by INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Method to raise the PropertyChanged event
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
