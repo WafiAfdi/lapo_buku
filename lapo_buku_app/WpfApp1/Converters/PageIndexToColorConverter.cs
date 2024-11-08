fda60dfeda41ad421b39e4198bf18f1ed74474ed
Wafi-Afdi <wafiafdi@gmail.com>
2024-11-08 18:09:28 +0700

    add : class relay command

---
+++ lapo_buku_app/WpfApp1/Converters/PageIndexToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp1.Converters
+{
+    public class PageIndexToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Ensure the value and parameter are integers
+            if (value is int pageIndex && parameter is int buttonIndex)
+            {
+                // Return a different color if the pageIndex matches the button index
+                return pageIndex == buttonIndex ? Brushes.IndianRed : Brushes.White;
+            }
+
+            return Brushes.White;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
