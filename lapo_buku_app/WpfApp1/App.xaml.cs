a1d4a8e3d9b0c03f5c268b9b9477cf881c72c3a5
Wafi-Afdi <wafiafdi@gmail.com>
2024-11-18 12:13:39 +0700

    update : ubah ke nama aplikasi lapo_buku

--- lapo_buku_app/WpfApp1/App.xaml.cs
+++ lapo_buku_app/WpfApp1/App.xaml.cs
@@ -74,7 +74,7 @@ namespace WpfApp1
 
         private INavigationService<TransaksiViewModel> CreateTransaksiNavigationService()
         {
-            return new LayoutNavigationService<TransaksiViewModel>(_navigationStore, () => new TransaksiViewModel(_authStore), CreateNavBarViewModel);
+            return new LayoutNavigationService<TransaksiViewModel>(_navigationStore, () => new TransaksiViewModel(_authStore, _dbConfig), CreateNavBarViewModel);
         }
 
         private NavbarViewModel CreateNavBarViewModel()
